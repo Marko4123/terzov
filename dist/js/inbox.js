f85007c2705404ce833d84613b8c601bfb0ed8f6
Марко Узунов <marko.uzunov@gmail.com>
2018-12-18 15:47:46 +0200

    adding ajax js

--- dist/js/inbox.js
+++ dist/js/inbox.js
@@ -1,22 +1,22 @@
-$(document).ready(function(e) {
-  $("#frmContact").on("submit", function(e) {
+$(document).ready(function (e) {
+  $("#frmContact").on("submit", function (e) {
     e.preventDefault();
     $("#loader-icon").show();
     var valid;
     valid = validateContact();
     if (valid) {
       $.ajax({
-        url: "../test/mail.php",
+        url: "/mail.php",
         type: "POST",
         data: new FormData(this),
         contentType: false,
         cache: false,
         processData: false,
-        success: function(data) {
+        success: function (data) {
           $("#mail-status").html(data);
           $("#loader-icon").hide();
         },
-        error: function() {}
+        error: function () { }
       });
     }
   });
